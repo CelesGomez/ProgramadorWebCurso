8045cea9343ed6dd540205c5ddc23b4ae392353d
Celeste Gomez <celugomez@gmail.com>
2023-06-17 21:56:43 -0300

    PROYECTO CERVECERIA

--- PROYECTO CERVECERIA/routes/index.js
+++ PROYECTO CERVECERIA/routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', async (req, res, next)=> {
+router.post('/', async (req, res, next) => {
 
   console.log(req.body)
 
@@ -18,25 +18,25 @@ router.post('/', async (req, res, next)=> {
   var mensaje = req.body.mensaje;
 
   var obj = {
-    to:'celugomez@gmail.com',
+    to: 'celugomez@gmail.com',
     subject: 'Contacto desde la web',
-    html: nombre + "" + apellido + "se contacto  a traves de la web y quiere mas info a este correo:" + email + ".<br>" Ademas , hizo el siguiente comentario: " + mensaje + ". <br> Su tel es " + telefono"}
+    html: nombre + " " + apellido + "se contacto  a traves de la web y quiere mas info a este correo:" + email + ".<br> Ademas, hizo el siguiente comentario: " + mensaje + ". < br > Su tel es " + telefono}
 
-    var transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      host: process.env.SMTP_PORT,
-      auth: {
-      host.process.env.SMTP_USER,
-      host.process.env.SMTP_PASS
-      }
-    })
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
 
-    var info = await transporter.sendMail(obj);
+  var info = await transport.sendMail(obj);
 
-    res.render('index', {
-      message: 'Mensaje enviado correctamente',
-    });
+  res.render('index', {
+    message: 'Mensaje enviado correctamente',
+  });
 
-})
+});
 
 module.exports = router;
